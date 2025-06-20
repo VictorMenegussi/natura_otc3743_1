3bf32f90e3332117db4e84ff6ccf4ba2ae396bbc
VictorMenegussi <victor.menegussi.t_numen@grupoboticario.com.br>
2025-06-20 13:47:03 -0300

    first commit

---
+++ parabaint/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("br.numen.parabaint.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
